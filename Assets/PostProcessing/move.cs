138ba3f9e50e239449bcdd0c50ca09cb2dc8857e
DESKTOP-219MNUS\kouki <standchat0820@gmail.com>
2017-05-25 22:35:33 +0900

    勉強会用サンプル

---
+++ Assets/PostProcessing/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    [SerializeField]
+    float m_Speed = 1000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = Time.deltaTime * m_Speed;
+        transform.Rotate(speed, 0, speed);
+	}
+}
